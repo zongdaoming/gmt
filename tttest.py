99310d088b7d56ddbb9d86858e1c6a7bc4221dee
Naive DorMin <m15901768536@163.com>
2023-08-31 17:44:19 +0800

    add whisperer

--- tttest.py
+++ tttest.py
@@ -1,3 +1,14 @@
+import argparse
 print("oll")
+# write a argument parser to get the input file name
 print("jianzhi cvpr!!!")
+# write a hello world program
+print("hello world!!!")
+# write a hello world program
+print("hello world!!!")
+# write a hello world program
+print("hello world!!!")
+args = argparse.ArgumentParser()
+args.add_argument("input_file_name")
+args.add_argument("output_file_name")
 
